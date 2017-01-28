74d4c3341b3fafff8eeddcdd34432eec95a5b061
NesDev <ludovic.goldak@gmail.com>
2017-01-28 02:49:34 +0100

    Modifs Samedi Matin

--- TP1/src/Parser/Main.java
+++ TP1/src/Parser/Main.java
@@ -1,34 +1,25 @@
 package Parser;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 import ADN.SequenceADN;
-<<<<<<< HEAD
-=======
 import IO.DotPlotWriter;
 import IO.FastaReader;
->>>>>>> bc6bf5615be102745e0ffe280fb6159655231e5b
 import Parser.ParametrageParser.TypeRecherche;
-import Reader.FastaReader;
 
 import javax.management.BadStringOperationException;
 
 public class Main {
 	public static void main(final String[] args) throws IOException, BadStringOperationException {
-		final SequenceADN sequenceADN = new FastaReader(args[0]).getSequence();
-		final ParametrageParser parametrage = new ParametrageParser();
-<<<<<<< HEAD
-		parametrage.addWordToParse("GATACA");
-		parametrage.setParam(TypeRecherche.DIRECT,TypeRecherche.REVERSE,TypeRecherche.COMPLEMENTAIRE,TypeRecherche.COMPLEMENTAIRE_REVERSE);
-		Parser parser
-	
-=======
+		//final SequenceADN sequenceADN = new FastaReader(args[0]).getSequence()
+        final SequenceADN sequenceADN = new FastaReader("TP1/fasta/ARNmessager-1.fasta").getSequence();
+        final ParametrageParser parametrage = new ParametrageParser();
+		//parametrage.setParam(TypeRecherche.DIRECT,TypeRecherche.REVERSE,TypeRecherche.COMPLEMENTAIRE,TypeRecherche.COMPLEMENTAIRE_REVERSE);
 		parametrage.setWordToParse("GU");
 		parametrage.setParam(TypeRecherche.DIRECT);
 		final ParserShiftOr parser = new ParserShiftOr(parametrage,sequenceADN);
 		final List<Integer> result = parser.runParser();
 		new DotPlotWriter(result,result).generateDotPlot("output.plot");
->>>>>>> bc6bf5615be102745e0ffe280fb6159655231e5b
 	}
 }
--- TP1/src/Parser/ParserNaif.java
+++ TP1/src/Parser/ParserNaif.java
@@ -1,23 +1,55 @@
 package Parser;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import ADN.SequenceADN;
+import Utils.StringUtils;
+
+import javax.management.BadStringOperationException;
 
 public class ParserNaif {
-	private ParametrageParser parametrageParser;
+	private static final char[] alphabet = {'A','C','G','T','U'};
+	private ParametrageParser parametrage;
 	private SequenceADN sequenceADN;
-	
-	public ParserNaif(SequenceADN seq, ParametrageParser params)
-	{
-		sequenceADN = seq;
-		parametrageParser = params;	
+
+	public ParserNaif(ParametrageParser parametrage, SequenceADN sequenceADN) {
+		this.parametrage = parametrage;
+		this.sequenceADN = sequenceADN;
 	}
-	
-	public ArrayList<Integer> GetIndexOccurence() 
-	{
-		ArrayList<Integer> maList = new ArrayList<Integer>();
-		
-		return maList;
+
+	/**
+	 * Recherche toutes les occurence du mot dans la séquence d'adn en fonction du paramétrage
+	 *
+	 * @return la liste de tous les indexes sans doublons.
+	 * @throws BadStringOperationException Est retourné si la séquence d'ADN contient des caractère ne se trouvant pas dans l'alphabet
+	 */
+	public List<Integer> runParser() throws BadStringOperationException {
+		Set<Integer> result = new HashSet<>();
+		if(parametrage.isDirect())
+			result.addAll(run(parametrage.getWordToParse()));
+		if(parametrage.isReverse())
+			result.addAll(run(StringUtils.reverse(parametrage.getWordToParse())));
+		if(parametrage.isComplementaire())
+			result.addAll(run(sequenceADN.getComplementaire(parametrage.getWordToParse())));
+		if(parametrage.isComplementaire_reverse()){
+			result.addAll(run(sequenceADN.getComplementaire(StringUtils.reverse(parametrage.getWordToParse()))));
+		}
+		return new ArrayList<>(result);
+	}
+
+	/**
+	 * Exécute la recherche du mot passé en paramètre dans la séquence d'ADN chargé.
+	 *
+	 * @param word Le mot à chercher
+	 * @return La liste des positions où se trouve le mot
+	 * @throws BadStringOperationException Est retourné si la séquence d'ADN contient des caractère ne se trouvant pas dans l'alphabet
+	 */
+	private List<Integer> run(String word) throws BadStringOperationException {
+		final List<Integer> result = new ArrayList<>();
+
+		return result;
 	}
 }
