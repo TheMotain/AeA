fdedfb99c8332633b5b8df595f931ee4f48a11ef
Dalencourt Alex <dalencourt@a12p8.fil.univ-lille1.fr>
2017-02-06 16:40:33 +0100

    refractor

--- TP1/src/Parser/ParserKMP.java
+++ TP1/src/Parser/ParserKMP.java
@@ -13,36 +13,10 @@ import java.util.Set;
  * Created by ludov on 29/01/2017.
  * Algorithme de Knuth, Morris et Pratt(1975)
  */
-public class ParserKMP {
-	private final ParametrageParser parametrage;
-	private final SequenceADN sequenceADN;
+public class ParserKMP extends AbstractParser{
 
 	public ParserKMP(final ParametrageParser parametrage, final SequenceADN sequenceADN) {
-		this.parametrage = parametrage;
-		this.sequenceADN = sequenceADN;
-	}
-
-	/**
-	 * Recherche toutes les occurence du mot dans la séquence d'adn en fonction du paramétrage
-	 *
-	 * @return la liste de tous les indexes sans doublons.
-	 *
-	 * @throws BadStringOperationException
-	 * 		Est retourné si la séquence d'ADN contient des caractère ne se trouvant pas dans l'alphabet
-	 */
-	public List<Integer> runParser() throws BadStringOperationException {
-		final Set<Integer> result = new HashSet<>();
-		if (parametrage.isDirect())
-			result.addAll(run(parametrage.getWordToParse()));
-		if (parametrage.isReverse())
-			result.addAll(run(StringUtils.reverse(parametrage.getWordToParse())));
-		if (parametrage.isComplementaire())
-			result.addAll(run(sequenceADN.getComplementaire(parametrage.getWordToParse())));
-		if (parametrage.isComplementaire_reverse()) {
-			result.addAll(run(sequenceADN.getComplementaire(StringUtils.reverse(parametrage.getWordToParse())
-			)));
-		}
-		return new ArrayList<>(result);
+		super(parametrage,sequenceADN);
 	}
 
 	/**
@@ -52,7 +26,7 @@ public class ParserKMP {
 	 * 		Le mot à chercher
 	 * @return La liste des positions où se trouve le mot
 	 */
-	private List<Integer> run(final String word) {
+	protected List<Integer> run(final String word) {
 		final List<Integer> result = new ArrayList<>();
 
 		return result;
