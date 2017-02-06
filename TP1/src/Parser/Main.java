d4ca940e7614fad358a034a85b0552061af51577
Dalencourt Alex <dalencourt@a12p8.fil.univ-lille1.fr>
2017-02-06 15:37:35 +0100

    gitignore

--- TP1/src/Parser/Main.java
+++ TP1/src/Parser/Main.java
@@ -2,17 +2,18 @@ package Parser;
 
 import ADN.SequenceADN;
 import IO.FastaReader;
-import Parser.ParametrageParser.TypeRecherche;
 
-import javax.management.BadStringOperationException;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
-	public static void main(final String[] args) throws IOException, BadStringOperationException {
+
+    public static void main(final String[] args) throws IOException {
+        final SequenceADN sequenceADN = new FastaReader(args[0]).getSequence();
+        final ParametrageParser parametrage = new ParametrageParser();
+        final Parser parser = null;
+    }
+
+	/*public static void main(final String[] args) throws IOException, BadStringOperationException {
 		//final SequenceADN sequenceADN = new FastaReader(args[0]).getSequence()
 		final SequenceADN sequenceADN = new FastaReader("fasta/ARNmessager-1.fasta").getSequence();
 		//final SequenceADN sequenceADN = new FastaReader("fasta/hsa-miR-20a_MI_ARN.fasta").getSequence();
@@ -34,6 +35,6 @@ public class Main {
             System.out.println("Clef : " + cle + " nbOccu : " + result.get(cle).size());
         }
         //new DotPlotWriter(result, result).generateDotPlot("output.plot");
-	}
+	}*/
 
 }
