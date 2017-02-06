22dd6100f3571964eb0214a729677cbae924f428
Dalencourt Alex <dalencourt@a12p8.fil.univ-lille1.fr>
2017-02-06 17:09:56 +0100

    jar

---
+++ TP1/script.sh
@@ -0,0 +1,4 @@
+java -jar TP1.jar fasta/chromosome13_NT_009952.14.fasta 5
+java -jar TP1.jar fasta/chromosome13_NT_009952.14.fasta 10
+java -jar TP1.jar fasta/chromosome13_NT_009952.14.fasta 20
+java -jar TP1.jar fasta/chromosome13_NT_009952.14.fasta 50
