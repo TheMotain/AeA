7711a1d9b7b0fa8bf0dc3d708a19d4b008dfe579
NesDev <ludovic.goldak@gmail.com>
2017-02-27 17:15:55 +0100

    Ajout main pour futur test

--- TP2/src/ADN/SequenceADN.java
+++ TP2/src/ADN/SequenceADN.java
@@ -3,6 +3,7 @@ package ADN;
 import javax.management.Query;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.Math;
 
 public class SequenceADN {
 	private String adnSequence;
@@ -10,27 +11,30 @@ public class SequenceADN {
 	public SequenceADN(){
 		adnSequence = "";
 	}
+	public SequenceADN(String _adnSequence){
+		this.adnSequence = _adnSequence;
+	}
 
 	public void append(final String line) {
 		adnSequence = adnSequence.concat(line);
 	}
 
-    /**
-     * Renvoi la taille de la séquence d'ADN
-     * @return taille
-     */
+	/**
+	 * Renvoi la taille de la séquence d'ADN
+	 * @return taille
+	 */
 	public int length(){
 		return adnSequence.length();
 	}
 
-    /**
-     * Renvoi le caractère à la position choisie
-     * @param index index du caractère
-     * @return le caractère
-     */
+	/**
+	 * Renvoi le caractère à la position choisie
+	 * @param index index du caractère
+	 * @return le caractère
+	 */
 	public char charAt(int index){
-	    return adnSequence.charAt(index);
-    }
+		return adnSequence.charAt(index);
+	}
 
 	/**
 	 * Permet de réaliser le complémentaire d'une séquence ADN
@@ -38,34 +42,34 @@ public class SequenceADN {
 	 * @return complémentaire
 	 */
 	public List<String> getComplementaires(String word) {
-        List<String> outputs = new ArrayList<>();
-        List<String> l1;
-        List<String> l2;
-        outputs.add("");
-	    for(char c : word.toCharArray()){
-            switch(c){
-                case 'A' :
-                    outputs = ajouter(outputs, 'U');
-                    break;
-                case 'T' :
-                    outputs = ajouter(outputs, 'A');
-                    break;
-                case 'C' :
-                    outputs = ajouter(outputs, 'G');
-                    break;
-                case 'G' :
-                    outputs = ajouter(outputs, 'C');
-                    break;
-                case 'U' :
-                    l1 = ajouter(outputs, 'A');
-                    l2 = ajouter(outputs, 'G');
-                    outputs.addAll(l1);
-                    outputs.addAll(l2);
-                    break;
-                default:
-                    outputs = ajouter(outputs, c);
-            }
-        }
+		List<String> outputs = new ArrayList<>();
+		List<String> l1;
+		List<String> l2;
+		outputs.add("");
+		for(char c : word.toCharArray()){
+			switch(c){
+				case 'A' :
+					outputs = ajouter(outputs, 'U');
+					break;
+				case 'T' :
+					outputs = ajouter(outputs, 'A');
+					break;
+				case 'C' :
+					outputs = ajouter(outputs, 'G');
+					break;
+				case 'G' :
+					outputs = ajouter(outputs, 'C');
+					break;
+				case 'U' :
+					l1 = ajouter(outputs, 'A');
+					l2 = ajouter(outputs, 'G');
+					outputs.addAll(l1);
+					outputs.addAll(l2);
+					break;
+				default:
+					outputs = ajouter(outputs, c);
+			}
+		}
 		return outputs;
 	}
 
@@ -75,12 +79,12 @@ public class SequenceADN {
 	 * @param a caractère à concaténer
 	 * @return nouvelle liste de complémentaires
 	 */
-    private List<String> ajouter(final List<String> inputs, final char a) {
-        List<String> outputs = new ArrayList<>();
-        for(String input : inputs){
-            outputs.add(input.concat(String.valueOf(a)));
-        }
-        return outputs;
+	private List<String> ajouter(final List<String> inputs, final char a) {
+		List<String> outputs = new ArrayList<>();
+		for(String input : inputs){
+			outputs.add(input.concat(String.valueOf(a)));
+		}
+		return outputs;
 	}
 
 
@@ -88,17 +92,17 @@ public class SequenceADN {
 	 * Getter de l'attribut {@link SequenceADN#adnSequence}
 	 * @return l'attribut
 	 */
-    public String getAdnSequence() {
-        return adnSequence;
-    }
+	public String getAdnSequence() {
+		return adnSequence;
+	}
 
 	/**
 	 * Setter de l'attribut {@link SequenceADN#adnSequence}
 	 * @param adnSequence l'attribut à setter
 	 */
 	public void setAdnSequence(final String adnSequence) {
-        this.adnSequence = adnSequence;
-    }
+		this.adnSequence = adnSequence;
+	}
 
 	/**
 	 * Retourne la taille de la séquence d'ADN
@@ -106,5 +110,15 @@ public class SequenceADN {
 	 */
 	public int getSize() {
 		return this.adnSequence.length();
+	}
+
+	/**
+	 * Retourne une séquence aléatoire de taille n présente dans la SequenceADN
+	 * @return Sequence de la SequenceADN
+	 */
+	public SequenceADN getRandomSequence(int n) {
+		if (n > this.adnSequence.length()) return new SequenceADN();
+		int i = (int) (Math.random() * this.adnSequence.length() - n + 1);
+		return new SequenceADN(this.adnSequence.substring(i,i + n));
 	}
 }
--- TP2/src/Utils/StringUtils.java
+++ TP2/src/Utils/StringUtils.java
@@ -8,4 +8,12 @@ public class StringUtils {
     public static String reverse (final String word){
         return new StringBuilder(word).reverse().toString();
     }
+
+    public static int countChar(final String chaine,final char aChar){
+        int count = 0;
+        for (int i = 0; i < chaine.length(); i++) {
+            if (chaine.charAt(i) == aChar) count++;
+        }
+        return  count;
+    }
 }
