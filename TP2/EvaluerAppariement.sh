c99c14245a7be9622760d271dd986e24f05850f0
alex <alex@debien.fr>
2017-03-12 14:43:33 +0100

    rapport

--- TP2/EvaluerAppariement.sh
+++ TP2/EvaluerAppariement.sh
@@ -3,6 +3,6 @@
 for microARN in `ls $1`; do
     echo "ARN messagé : $microARN"
     echo "\nRésultat:\n"
-    java -jar Apparaiment.jar $1/${microARN}
+    java -jar Appariement.jar $1/${microARN}
     echo "\n------------------------------------------------------------------\n"
 done
