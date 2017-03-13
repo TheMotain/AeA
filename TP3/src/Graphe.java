40d381a24162b7b40400c22598a0c9473b720394
dalencourt <alex.dalencourt@gmail.com>
2017-03-13 15:30:50 +0100

    init TP3

---
+++ TP3/src/Graphe.java
@@ -0,0 +1,6 @@
+/**
+ * Représente la liste des mots avec les liaisons associées<br/>
+ * Created by dalencourt on 13/03/17.
+ */
+public class Graphe {
+}
---
+++ TP3/src/Liste.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+/**
+ * Représente une liste d'arrètes de destination pour un graphe orienté <br/>
+ *
+ * Created by dalencourt on 13/03/17.
+ */
+public class Liste {
+    private List<Integer> dest;
+}
