0d146dcb94f77dd259b1627988d2f2e28c548040
NesDev <ludovic.goldak@gmail.com>
2017-03-13 16:05:20 +0100

    constructeur graph

--- TP3/src/Graphe/Graphe.java
+++ TP3/src/Graphe/Graphe.java
@@ -5,16 +5,13 @@ package Graphe;
  * Created by dalencourt on 13/03/17.
  */
 public class Graphe {
-    Liste listeSucc[];
+    Successeur listeSucc[];
     String mot[];
     int nbMot = 0;
     public Graphe(String[] lesMots){
         mot = lesMots.clone();
         nbMot = lesMots.length;
-        listeSucc = new Liste[nbMot];
-        for (int i = 0; i < lesMots.length; i++) {
-            listeSucc[i] = new Liste()
-        }
+        listeSucc = new Successeur[nbMot];
     }
 
     public Successeur getSuccesseur(int s) {
