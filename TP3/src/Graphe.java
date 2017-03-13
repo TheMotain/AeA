96c3af734656fb611cf6418ef1ad566401516a11
NesDev <ludovic.goldak@gmail.com>
2017-03-13 15:43:44 +0100

    constructeur graph

--- TP3/src/Graphe.java
+++ TP3/src/Graphe.java
@@ -3,6 +3,15 @@
  * Created by dalencourt on 13/03/17.
  */
 public class Graphe {
+    Liste listeSucc[];
+    String mot[];
+    int nbMot = 0;
+    public Graphe(String[] lesMots){
+        mot = lesMots.clone();
+        nbMot = lesMots.length;
+        for (int i = 0; i < lesMots.length; i++) {
+        }
+    }
 
     public void getSuccesseur(int s) {
     }
